3ad9149129c70082e1870b4f24a3adfc05fcc3ac
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-12 15:02:40 +0100

    addEventListener

--- dom.js
+++ dom.js
@@ -2,3 +2,5 @@ const isOffLamp = document.getElementById("off-lamp");
 console.log("off-lamp");
 const myButton = document.getElementById("btn");
 console.log("btn btn-primary");
+
+document.addEventListener("click", function () {});
