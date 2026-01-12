f818ecd618fed223800789e42ab893a34722468c
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-12 15:38:39 +0100

    add second img

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 const isOffLamp = document.getElementById("off-lamp");
 console.log("off-lamp");
+const isOnLamp = document.getElementById("on-lamp");
+console.log("on-lamp");
 const myButton = document.getElementById("btn");
 console.log("btn btn-primary");
 
