e80dc365aa08648ce4cf7e3a474b9bfd7ba5327f
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-12 16:10:16 +0100

    delete second img

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
 const isOffLamp = document.getElementById("off-lamp");
 console.log("off-lamp");
-const isOnLamp = document.getElementById("on-lamp");
-console.log("on-lamp");
 const myButton = document.getElementById("btn");
 console.log("btn btn-primary");
 
-document.addEventListener("click", function () {});
+myButton.addEventListener("click", function () {
+  console.log("ho cliccato");
+});
